890a97c8fc42d610154393f7abb9c249ce5c7f17
Y2kz <39020251+Y2kz@users.noreply.github.com>
2022-07-24 23:36:04 +0530

    created setup.sh
    created setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "installing Figlet"
+#for debian based (modify here for other base distro)
+sudo apt install figlet -y
+echo "Settting scripts executable"
+chmod +x get_content.sh 
+chmod +x get_content.sh 
+echo "Finished Setup"
+echo "Run server.sh now"
