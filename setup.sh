9b7b416d50e1fdb9d90ef67854063228799bac27
Y2kz <39020251+Y2kz@users.noreply.github.com>
2022-07-24 23:40:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "installing Figlet"
 sudo apt install figlet -y
 echo "Settting scripts executable"
 chmod +x get_content.sh 
-chmod +x get_content.sh 
+chmod +x server.sh
+
 echo "Finished Setup"
 echo "Run server.sh now"
