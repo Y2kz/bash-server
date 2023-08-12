a715ed5c4c3facc08f5f2928c777bf557a32664d
Y2kz <39020251+Y2kz@users.noreply.github.com>
2023-08-13 01:10:48 +0530

    Update setup.sh
    removed figlet dependency

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-echo "installing Figlet"
+#echo "installing Figlet"
 #for debian based (modify here for other base distro)
-sudo apt install figlet -y
+#sudo apt install figlet -y
+
+#install netcat package if not installed
+#yum install nc
+apt install nc
 echo "Settting scripts executable"
 chmod +x get_content.sh 
 chmod +x server.sh
